7150a3950183fc24224f077b0c380c0fbdd2ff6d
V R Rishendra <98251067+VRRISHENDRA@users.noreply.github.com>
2023-12-02 19:50:39 +0530

    Add files via upload

---
+++ Write.py
@@ -0,0 +1,10 @@
+#CODE TO WRITE TO THE RFID CARD
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+rfid = SimpleMFRC522()
+try:
+ print("Hold tag near the module...")
+ rfid.write("Circuit basics")
+ print("Written")
+finally:
+ GPIO.cleanup()
